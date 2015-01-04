5d52a0fd686021bde7243f6038c8c209685f45bd
Mike Golubitsky <mike617@gmail.com>
2015-01-04 14:40:20 -0800

    has_one_through incomplete

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -31,41 +31,71 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
+    self.class_name = options[:class_name] || name.to_s.camelize
     self.foreign_key = options[:foreign_key] || "#{name}_id".intern
     self.primary_key = options[:primary_key] || "id".intern
-    self.class_name = options[:class_name] || name.camelize
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
+    self.class_name = options[:class_name] || name.to_s.singularize.camelize
     self.foreign_key = options[:foreign_key] ||
       "#{self_class_name.underscore}_id".intern
     self.primary_key = options[:primary_key] || "id".intern
-    self.class_name = options[:class_name] || name.singularize.camelize
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    options = BelongsToOptions(name, options)
+    options = BelongsToOptions.new(name, options)
+    assoc_options[name] = options
+    
     define_method(name) do
-      #stopped here for now
-      # options.model_class.send(:foreign_key)
+      return nil unless send(options.foreign_key)
 
+      result = DBConnection.execute(<<-SQL)
+        SELECT
+          *
+        FROM
+          #{options.table_name}
+        WHERE
+          #{options.primary_key} = #{send(options.foreign_key)}
+      SQL
+
+      options.model_class.new(result.first)
     end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self.to_s, options)
+
+    define_method(name) do
+      results = DBConnection.execute(<<-SQL)
+        SELECT
+          *
+        FROM
+          #{options.table_name}
+        WHERE
+          #{options.foreign_key} = #{send(options.primary_key)}
+      SQL
+
+      objects = []
+      results.each do |result|
+        objects << options.model_class.new(result)
+      end
+      objects
+    end
   end
 
   def assoc_options
     # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
   end
 end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -5,6 +5,29 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    define_method(name) do
+      results = DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.class.assoc_options[source_name].table_name}
+      JOIN
+        #{self.class.assoc_options[through_name].table_name}
+      ON
+        #{self.class.assoc_options[through_name].foreign_key} =
+          #{self.class.assoc_options[source_name].primary_key}
+      WHERE
+        #{self.class.assoc_options[source_name].primary_key} =
+          #{send(self.class.assoc_options[through_name].foreign_key)}
+      SQL
+
+      objects = []
+      results.each do |result|
+        objects << options.model_class.new(result)
+      end
+      objects
+    end
+
   end
+
 end
